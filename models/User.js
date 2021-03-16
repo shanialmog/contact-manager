6d4817fabdce5d2f56189efda10877cb897517bf
Shani Almog <killerdrum@gmail.com>
2021-03-16 18:55:35 +0200

    write the users route

--- models/User.js
+++ models/User.js
@@ -3,6 +3,21 @@ const mongoose = require('mongoose')
 const UserSchema = mongoose.Schema({
     name: {
         type: String,
-        reuire: true
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     },
 })
+
+module.exports = mongoose.model('user', UserSchema)
