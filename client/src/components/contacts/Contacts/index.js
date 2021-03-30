7b1e140022242ad8a322320effc3545ca1c27a2c
Shani Almog <killerdrum@gmail.com>
2021-03-30 18:46:07 +0300

    implement animation affects in contacts appearance

--- client/src/components/contacts/Contacts/index.js
+++ client/src/components/contacts/Contacts/index.js
@@ -1,5 +1,6 @@
 import { Fragment, useContext } from 'react'
 import ContactContext from '../../../context/contact/contactContext'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import ContactItem from '../ContactItem'
 
 const Contacts = () => {
@@ -13,17 +14,23 @@ const Contacts = () => {
 
     return (
         <Fragment>
-            {filtered !== null ?
-                filtered.map(
-                    (contact) => (
-                        <ContactItem key={contact.id} contact={contact} />
-                    ))
-                :
-                contacts.map(
-                    contact =>
-                        <ContactItem key={contact.id} contact={contact} />
-                )
-            }
+            <TransitionGroup>
+                {filtered !== null ?
+                    filtered.map(
+                        (contact) => (
+                            <CSSTransition key={contact.id} timeout={400} classNames="item">
+                                <ContactItem contact={contact} />
+                            </CSSTransition>
+                        ))
+                    :
+                    contacts.map(
+                        contact => (
+                            <CSSTransition key={contact.id} timeout={400} classNames="item">
+                                <ContactItem contact={contact} />
+                            </CSSTransition>
+                        ))
+                }
+            </TransitionGroup>
         </Fragment>
     )
 }
